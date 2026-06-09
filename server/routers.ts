48e34593e8f1ab53bb878cc8bae997c51e48036a
SAJJADSLK <56158654+SAJJADSLK@users.noreply.github.com>
2026-06-09 23:44:10 +0400

    Update routers.ts

--- server/routers.ts
+++ server/routers.ts
@@ -1,6 +1,9 @@
+import { COOKIE_NAME } from "@shared/const";
+
 import { getSessionCookieOptions } from "./_core/cookies.js";
 import { systemRouter } from "./_core/systemRouter.js";
 import { publicProcedure, router } from "./_core/trpc.js";
+
 import { timeRouter } from "./routers/time.js";
 import { citiesRouter } from "./routers/cities.js";
 import { favoritesRouter } from "./routers/favorites.js";
@@ -9,18 +12,28 @@ import { meetingsRouter } from "./routers/meetings.js";
 import { countdownsRouter } from "./routers/countdowns.js";
 
 export const appRouter = router({
-    // if you need to use socket.io, read and register route in server/_core/index.ts, all api should start with '/api/' so that the gateway can route correctly
+  // if you need to use socket.io, read and register route in server/_core/index.ts
+  // all api should start with '/api/' so that the gateway can route correctly
+
   system: systemRouter,
+
   auth: router({
-    me: publicProcedure.query(opts => opts.ctx.user),
+    me: publicProcedure.query((opts) => opts.ctx.user),
+
     logout: publicProcedure.mutation(({ ctx }) => {
       const cookieOptions = getSessionCookieOptions(ctx.req);
-      ctx.res.clearCookie(COOKIE_NAME, { ...cookieOptions, maxAge: -1 });
+
+      ctx.res.clearCookie(COOKIE_NAME, {
+        ...cookieOptions,
+        maxAge: -1,
+      });
+
       return {
         success: true,
       } as const;
     }),
   }),
+
   time: timeRouter,
   cities: citiesRouter,
   favorites: favoritesRouter,
