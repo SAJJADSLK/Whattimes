35807100f0d3e358116b942bc35e9e2508fa0bc5
SAJJADSLK <56158654+SAJJADSLK@users.noreply.github.com>
2026-06-10 15:39:35 +0400

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,8 +1,8 @@
 import { eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import { InsertUser, users } from "../drizzle/schema";
-import { ENV } from "./_core/env";
+import { InsertUser, users } from "../drizzle/schema.js";
+import { ENV } from "./_core/env.js";
 
 type PostgresClient = ReturnType<typeof postgres>;
 type DbClient = ReturnType<typeof drizzle>;
