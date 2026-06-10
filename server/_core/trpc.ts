d549d4303738428841724488813b925f9ef382f6
SAJJADSLK <56158654+SAJJADSLK@users.noreply.github.com>
2026-06-10 23:01:58 +0400

    Update trpc.ts

--- server/_core/trpc.ts
+++ server/_core/trpc.ts
@@ -9,33 +9,6 @@ const t = initTRPC.context<TrpcContext>().create({
 export const router = t.router;
 export const publicProcedure = t.procedure;
 
-// Authentication removed
+// Auth removed
 export const protectedProcedure = t.procedure;
 export const adminProcedure = t.procedure;
-
-  return next({
-    ctx: {
-      ...ctx,
-      user: ctx.user,
-    },
-  });
-});
-
-export const protectedProcedure = t.procedure.use(requireUser);
-
-export const adminProcedure = t.procedure.use(
-  t.middleware(async opts => {
-    const { ctx, next } = opts;
-
-    if (!ctx.user || ctx.user.role !== 'admin') {
-      throw new TRPCError({ code: "FORBIDDEN", message: NOT_ADMIN_ERR_MSG });
-    }
-
-    return next({
-      ctx: {
-        ...ctx,
-        user: ctx.user,
-      },
-    });
-  }),
-);
