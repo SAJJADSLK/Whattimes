50fe5b35670f935f3e182c470574cfad9eb349c6
SAJJADSLK <56158654+SAJJADSLK@users.noreply.github.com>
2026-06-10 14:39:50 +0400

    Update routers.ts

--- server/routers.ts
+++ server/routers.ts
@@ -1,4 +1,4 @@
-import { COOKIE_NAME } from "../../shared/const.js";
+import { COOKIE_NAME } from "../../shared/const.ts";
 
 import { getSessionCookieOptions } from "./_core/cookies.js";
 import { systemRouter } from "./_core/systemRouter.js";
