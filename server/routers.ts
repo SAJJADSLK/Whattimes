31d059d0d4d8a8cc7a76d0d6bc954c50efa2f067
SAJJADSLK <56158654+SAJJADSLK@users.noreply.github.com>
2026-06-11 14:18:37 +0400

    Update routers.ts

--- server/routers.ts
+++ server/routers.ts
@@ -1,19 +1,10 @@
 import { systemRouter } from "./_core/systemRouter.js";
-import { publicProcedure, router } from "./_core/trpc.js";
+import { router } from "./_core/trpc.js";
 import { timeRouter } from "./routers/time.js";
 import { citiesRouter } from "./routers/cities.js";
 
 export const appRouter = router({
   system: systemRouter,
-
-  auth: router({
-    me: publicProcedure.query(() => null),
-
-    logout: publicProcedure.mutation(() => ({
-      success: true,
-    })),
-  }),
-
   time: timeRouter,
   cities: citiesRouter,
 });
